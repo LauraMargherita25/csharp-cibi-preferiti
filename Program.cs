9c0b9a2bd081b5c8ae225558ee09159ada45c317
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-06-23 15:46:25 +0200

    Ask favourite food

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+// New array
 string[] favoriteFoods = { "pizza", "carbonara", "tiramisù", "takoyaki", "gyoza", "pasta al pesto", "fiorentina", "ramen" };
 
+// Array length 
+Console.Write("\nTotal Number of Elements: ");
 Console.WriteLine(favoriteFoods.Length);
 
+// Listing array
+Console.WriteLine("List of favourite foods:");
 foreach (string food in favoriteFoods)
     Console.WriteLine(food);
 
-Console.WriteLine("primo in classifica: " + favoriteFoods[0]);
-Console.WriteLine("ultimo in classifica: " + favoriteFoods.Last());
-Console.WriteLine("a metà classifica: " + favoriteFoods[favoriteFoods.Length / 2 - 1] + " " + favoriteFoods[favoriteFoods.Length / 2]);
+// First Middle Last positions
+Console.WriteLine("first in the standings: " + favoriteFoods[0]);
+Console.WriteLine("last in the standings: " + favoriteFoods.Last());
+Console.WriteLine("half in the standigs: " + favoriteFoods[favoriteFoods.Length / 2 - 1] + " " + favoriteFoods[favoriteFoods.Length / 2]);
+
+//Ask fvorite food
+Console.WriteLine("Enter yours favorite food:");
+
+string userFavoriteFood = Console.ReadLine();
+
+Console.WriteLine("User favorite food is: " + userFavoriteFood);
 
