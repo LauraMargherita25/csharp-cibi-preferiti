a6e48a2b67b54d271ecc9c28c1ab0618dd75f4ff
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-06-23 15:19:52 +0200

    Done with bonus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] favoriteFoods = { "pizza", "carbonara", "tiramisù", "takoyaki", "gyoza", "pasta al pesto", "fiorentina", "ramen" };
+
+Console.WriteLine(favoriteFoods.Length);
+
+foreach (string food in favoriteFoods)
+    Console.WriteLine(food);
+
+Console.WriteLine("primo in classifica: " + favoriteFoods[0]);
+Console.WriteLine("ultimo in classifica: " + favoriteFoods.Last());
+Console.WriteLine("a metà classifica: " + favoriteFoods[favoriteFoods.Length / 2 - 1] + " " + favoriteFoods[favoriteFoods.Length / 2]);
+
